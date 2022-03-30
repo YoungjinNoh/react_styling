11deb7f634b06b2cd5cd5dd779838122da68b200
YoungjinNoh <youngjin_noh@naver.com>
2022-03-30 15:26:17 +0900

    styling part end

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import SassComponent from './SassComponent';
+import CSSModule from './CSSModule';
+import StyledComponent from './StyledComponent';
 
 function App() {
   return (
-    <SassComponent/>
+    <StyledComponent/>
   );
 }
 
